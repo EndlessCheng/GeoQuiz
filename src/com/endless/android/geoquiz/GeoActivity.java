e985f3d74813ce40a4edd120fb068cc5b6b2638b
EndlessCheng <loli.con@qq.com>
2014-07-04 13:52:34 +0800

    添加组件及设置事件

--- src/com/endless/android/geoquiz/GeoActivity.java
+++ src/com/endless/android/geoquiz/GeoActivity.java
@@ -1,23 +1,51 @@
 package com.endless.android.geoquiz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GeoActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_geo);
-    }
+	private Button mTrueButton;
+	private Button mFalseButton;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_geo);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.geo, menu);
-        return true;
-    }
-    
+		mTrueButton = (Button) findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(GeoActivity.this, R.string.correct_toast,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		mFalseButton = (Button) findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(GeoActivity.this, R.string.incorrect_toast,
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.geo, menu);
+		return true;
+	}
+
 }
