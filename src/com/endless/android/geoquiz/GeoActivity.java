56a9d1d342ffa04e5d72e3f51eca97c3307a9e89
EndlessCheng <loli.con@qq.com>
2014-08-10 16:15:25 +0800

    添加 Next 按钮

--- src/com/endless/android/geoquiz/GeoActivity.java
+++ src/com/endless/android/geoquiz/GeoActivity.java
@@ -52,6 +52,17 @@ public class GeoActivity extends Activity {
 						Toast.LENGTH_SHORT).show();
 			}
 		});
+
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				int question = mQuestionBank[mCurrentIndex].getQuestion();
+				mQuestionTextView.setText(question);
+			}
+		});
 	}
 
 	@Override
