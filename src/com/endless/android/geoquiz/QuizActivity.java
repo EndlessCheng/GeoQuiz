078b03b291d6e43005adc81115ecb6374f2f3ad6
EndlessCheng <loli.con@qq.com>
2014-08-11 12:59:58 +0800

    修改名字

---
+++ src/com/endless/android/geoquiz/QuizActivity.java
@@ -0,0 +1,106 @@
+package com.endless.android.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends Activity {
+
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mPrevButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_me, true),
+			new TrueFalse(R.string.question_you, false),
+			new TrueFalse(R.string.question_java, true),
+			new TrueFalse(R.string.question_cplusplus, true),
+			new TrueFalse(R.string.question_python, true) };
+
+	private int mCurrentIndex = 0;
+
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		Toast.makeText(
+				this,
+				answerIsTrue == userPressedTrue ? R.string.correct_toast
+						: R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+
+		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+
+		mTrueButton = (Button) findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				checkAnswer(true);
+			}
+		});
+
+		mFalseButton = (Button) findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+			}
+		});
+
+		
+		mPrevButton = (Button) findViewById(R.id.prev_button);
+		mPrevButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+
+		updateQuestion();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.geo, menu);
+		return true;
+	}
+
+}
